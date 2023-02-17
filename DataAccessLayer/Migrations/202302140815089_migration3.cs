4410489c5a706eed6c1ba973334ac13d0cfcb4e1
Mehmet Berk BAYTÜRK <mberkbayturk@gmail.com>
2023-02-17 10:28:30 +0300

    Category table updated

---
+++ DataAccessLayer/Migrations/202302140815089_migration3.cs
@@ -0,0 +1,18 @@
+﻿namespace DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class migration3 : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.Categories", "CategoryStatus");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Categories", "CategoryStatus", c => c.Boolean(nullable: false));
+        }
+    }
+}
